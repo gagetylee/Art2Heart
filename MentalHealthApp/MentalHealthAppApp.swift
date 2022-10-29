7b60ae55fa851a1f2ac0a3aa963f271e91bbd6b1
Gage Tylee <gagetylee@gmail.com>
2022-10-29 15:31:54 -0700

    Initial Commit

---
+++ MentalHealthApp/MentalHealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentalHealthAppApp.swift
+//  MentalHealthApp
+//
+//  Created by Gage Tylee on 2022-10-29.
+//
+
+import SwiftUI
+
+@main
+struct MentalHealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
